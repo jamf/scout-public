1966294b077bd5ceaa4dc0d6bf6ac10869ab6a27
Jacob Schultz <schultz.a.jacob@gmail.com>
2019-06-04 22:45:35 -0500

    UI to update cron jobs
    Adds the UI to the settings page to manually sync the cron jobs

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -8,6 +8,7 @@ function getOrgDetails(){
     if ('cron_jobs' in detailsObject){
       window.all_crons = detailsObject.cron_jobs;
       var scoutTable = $("#cron-jobs-table").DataTable();
+      scoutTable.clear();
       for (i = 0; i < detailsObject.cron_jobs.length; i++) {
         let cronDetails = detailsObject.cron_jobs[i].split(' ');
         const button = '<button onclick="viewFullCronString('+i+');" type="button" class="btn btn-info btn-circle"><i class="fa fa-eye"></i></button>';
@@ -25,6 +26,27 @@ function viewFullCronString(index) {
   swal('Full Cron String: ',
     window.all_crons[i],
     'success');
+}
+
+function updateCronJobsOnScoutServer() {
+  swal({
+    title: "Confirm Action",
+    text: "Are you sure you want to manually refresh the scout cron jobs? This will update the cron jobs with all jamf pro servers in the database. This writes directly to the system crontab file and can't be undone.",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true
+  })
+  .then((willSync) => {
+    if (willSync){
+      var cronJobs = getRequestObject('/settings/cronjobs', null, 'PUT');
+      cronJobs.done(function(result){
+        swal('Cron jobs updated!', 'The cron jobs in the scout databse have been syncronized to the server. Verify they are correct in the cron job table. ','success');
+      })
+      .fail(function(xhr){
+        swal('Error', 'Scout was unable to update the cron jobs on the scout server. Please try restarting the server.','error');
+      });
+    }
+  });
 }
 
 function refreshAllDevices(showPrompt){

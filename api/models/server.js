49ecbbf33e45cdce76610513c6d8ff3e06a43475
alex-stout <stoutab4384@uwec.edu>
2019-07-29 19:18:01 -0500

    removes non-ascii chars from string

--- api/models/server.js
+++ api/models/server.js
@@ -383,10 +383,15 @@ function translateAPIDeviceToDatabaseObject(device, serverId, type){
   } else {
     lastDate = device.report_date_utc;
   }
-  var dbObject = {"server_id" : serverId, "device_type" : type, "jss_id" : device.id, "jss_name" : device.name,
+  var dbObject = {"server_id" : serverId, "device_type" : type, "jss_id" : device.id, "jss_name" : cleanInput(device.name),
                   "jss_serial" : device.serial_number, "jss_last_inventory" : lastDate,
                   "jss_model" : device.model, "jss_managed" : device.managed, "jss_udid" : device.udid};
   return dbObject;
+}
+
+function cleanInput(dirtyString) {
+  // clean non-ascii characters from string
+  return dirtyString.replace(/[^\x00-\x7F]/g, "");
 }
 
 exports.getAllDevices = function(url, serverId, username, password){

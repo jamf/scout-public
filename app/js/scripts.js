0a75ad3301af8d920850f24e475bd2e1e36c03fd
Foster <foster.morgan11@gmail.com>
2019-11-30 17:11:23 -0600

    saved reports in a table on the dashboard

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -167,13 +167,47 @@ function loadReportById(reportId){
 }
 
 function loadReportsWithDashboard(){
-  // reloadReportPane(false);
+  reloadReportPane(false);
   //Make the request to the server to get a saved report
   var report = getRequestObject('/reports/dashboard/', null, 'GET');
   report.done(function(reportObject){
     console.log(reportObject);
-    // foreach(report)
-    // $("#reportsToShow").val();
+    // EXTRACT VALUE FOR HTML HEADER. following is copy pasted from a stack overflow page, 
+    // ('Book ID', 'Book Name', 'Category' and 'Price')
+    var col = [];
+    for (var i = 0; i < reportObject.length; i++) {
+        for (var key in reportObject[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < reportObject.length; i++) {
+        tr = table.insertRow(-1);
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = reportObject[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("reports-dashboard-table");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
   })
   .fail(function(xhr){
     console.log(xhr);

647c7fddbcf33e8cd1b701a2b069a8868fc117d1
alex-stout <stoutab4384@uwec.edu>
2019-12-04 14:49:31 -0600

    fix fields to select population

--- api/controllers/reports.js
+++ api/controllers/reports.js
@@ -69,6 +69,7 @@ reports.get('/id/:reportId', function(req,res){
   //Get the report and it's line items from the database
   report.getReportById(req.params.reportId)
   .then(function(report){
+    console.log(report)
     return res.status(200).send(report);
   })
   .catch(error => {
